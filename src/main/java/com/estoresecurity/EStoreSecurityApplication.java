0e39d9bf3ab802cc378ab817ec38474ea8d5d68c
Ankit <chaudharyankit3@gmail.com>
2022-09-02 21:32:41 +0530

    Basic Workspace

---
+++ src/main/java/com/estoresecurity/EStoreSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.estoresecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EStoreSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EStoreSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estoresecurity/EStoreSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoresecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EStoreSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
